5ed348aba24002b9e95f7dd6f9581b8fb445728e
Alideveloping <alihaddadi8016@gmail.com>
2024-06-12 22:35:47 +0330

    Add project files.

---
+++ CrudRepos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudRepos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
